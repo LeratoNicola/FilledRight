5499797f9af3649dd858d2d10dd002596364f5c3
LeratoNicola <ST10463610@rcconnect.edu.za>
2025-03-30 19:41:12 +0200

    Edited greeting message and inserted 2 new columns for it.

--- app/src/main/java/com/example/filledright/MainActivity.kt
+++ app/src/main/java/com/example/filledright/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.filledright
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,15 +18,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FilledRightTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Hera")
-                }
-            }
+           Column {
+             Text(text = "Welcome to Filled Right. Assisting with your dietary needs")
+             Text(text = "Hello Hera. Excited to be with you on this journey")
+
+               
+
+           }
         }
     }
 }
